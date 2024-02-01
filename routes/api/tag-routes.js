fdec9b20a155108a318dbac18569e6cff1136b30
Karra <karra.maynard@gmail.com>
2024-02-01 15:35:43 -0500

    GET route to find all tags and associated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,8 +5,21 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
-});
+  // include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      }
+    ]
+  })
+  .then(tags => res.json(tags))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json(err);
+  });
+  });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
